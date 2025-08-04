11e57cef144e5ed83938e74281ba1fdf84ece627
Yuuenn <94096560+Yuuenn@users.noreply.github.com>
2025-08-04 11:18:24 +0300

    del unexpected files

--- test-error/target/debug/deps/test_error-d4503041e02f1655.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ianma96\cargo-zh\test-error\target\debug\deps\test_error-d4503041e02f1655.d: src\main.rs
-
-C:\Users\ianma96\cargo-zh\test-error\target\debug\deps\libtest_error-d4503041e02f1655.rmeta: src\main.rs
-
-src\main.rs:
